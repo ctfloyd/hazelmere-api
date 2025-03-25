34c1655ae97972b18f1f5850b61d9c2bb35c09c8
ctfloyd <calebfloyd0899@gmail.com>
2025-03-24 19:21:14 -0500

    attempt to emit metrics through middleware

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -4,7 +4,6 @@ import (
 	"api/src/internal"
 	"api/src/internal/common/logger"
 	"context"
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"net/http"
 	"os"
 	"os/signal"
@@ -20,7 +19,7 @@ func main() {
 	app.Init(ctx, l)
 
 	l.Info(ctx, "Trying listen and serve 8080.")
-	err := http.ListenAndServe(":8080", otelhttp.NewHandler(app.Router, "hazelmere-api"))
+	err := http.ListenAndServe(":8080", app.Router)
 	if err != nil {
 		l.InfoArgs(ctx, "Failed to listen and serve on port 8080: %v", err)
 	}
--- src/internal/initialize/router.go
+++ src/internal/initialize/router.go
@@ -2,6 +2,7 @@ package initialize
 
 import (
 	"api/src/internal/common/logger"
+	"api/src/internal/common/metrics"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"time"
@@ -10,8 +11,9 @@ import (
 func InitRouter(log logger.Logger) *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Recoverer)
-	router.Use(middleware.RequestID)
 	router.Use(middleware.Timeout(1 * time.Second))
+	router.Use(middleware.RequestID)
+	router.Use(metrics.Middleware)
 	router.Use(logger.NewMiddleware(log).Serve)
 	return router
 }
