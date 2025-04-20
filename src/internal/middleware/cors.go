c48ada3d86c8ec22d10ffed9454d33c13833958f
ctfloyd <calebfloyd0899@gmail.com>
2025-04-20 10:43:28 -0500

    allow cors methods

--- src/internal/middleware/cors.go
+++ src/internal/middleware/cors.go
@@ -7,6 +7,7 @@ import (
 func AllowCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		next.ServeHTTP(w, r)
 	})
 }
