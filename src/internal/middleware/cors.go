56f4db4cab9d6a96070dcb06d098d1e91586c199
ctfloyd <calebfloyd0899@gmail.com>
2025-04-20 10:47:36 -0500

    more cors

--- src/internal/middleware/cors.go
+++ src/internal/middleware/cors.go
@@ -7,7 +7,8 @@ import (
 func AllowCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		next.ServeHTTP(w, r)
 	})
 }
