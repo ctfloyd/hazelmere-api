5e5678733b6e855d5649298c54907bbe361bb8c3
ctfloyd <calebfloyd0899@gmail.com>
2025-04-20 10:24:25 -0500

    allow cors

--- src/internal/initialize/router.go
+++ src/internal/initialize/router.go
@@ -1,15 +1,17 @@
 package initialize
 
 import (
+	"github.com/ctfloyd/hazelmere-api/src/internal/middleware"
 	"github.com/ctfloyd/hazelmere-commons/pkg/hz_logger"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	chiWare "github.com/go-chi/chi/v5/middleware"
 )
 
 func InitRouter(log hz_logger.Logger) *chi.Mux {
 	router := chi.NewRouter()
-	router.Use(middleware.Recoverer)
-	router.Use(middleware.RequestID)
+	router.Use(middleware.AllowCors)
+	router.Use(chiWare.Recoverer)
+	router.Use(chiWare.RequestID)
 	router.Use(hz_logger.NewMiddleware(log).Serve)
 	return router
 }
